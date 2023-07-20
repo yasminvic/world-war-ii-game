c1f5c9a8fef29ea9f408a7103d2b63a31fbe2bf8
Yasmin <yasminalvesdesouza27@gmail.com>
2023-07-20 14:31:23 -0300

    Movimentação do Personagem

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Move")]
+    [SerializeField]
+    private float moveSpeed = 0.1f;
 
-    // Update is called once per frame
+    [Header("Bounds")]
+    [SerializeField]
+    private BoxCollider2D playerBounds;
+
     void Update()
     {
-        
+        //controla a movimentação
+        Move();
+
+        //limite da tela
+        ApplyBounds();
+    }
+
+    void Move()
+    {
+        var v = Input.GetAxis("Vertical");
+        var h = Input.GetAxis("Horizontal");
+
+        var move = new Vector3
+        (
+            h * moveSpeed * Time.deltaTime,
+            v * 0
+        );
+
+
+        transform.Translate(move);
+    }
+
+    void ApplyBounds()
+    {
+        var minX = -playerBounds.bounds.extents.x + playerBounds.offset.x;
+        var maxX = playerBounds.bounds.extents.x + playerBounds.offset.x;
+
+        var minY = -playerBounds.bounds.extents.y + playerBounds.offset.y;
+        var maxY = playerBounds.bounds.extents.y + playerBounds.offset.y;
+
+        //define a posição do player
+        transform.position = new Vector3(
+            //vai garantir que o valor de x e y tenha um min e max
+            Mathf.Clamp(transform.position.x, minX, maxX),
+            Mathf.Clamp(transform.position.y, minY, maxY)
+        );
     }
 }
