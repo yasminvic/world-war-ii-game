16df145c077774c9fd1e21351949882964297575
Yasmin <yasminalvesdesouza27@gmail.com>
2023-07-23 15:21:05 -0300

    Feat: Explosion animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-
+    [Header("Prefabs")]
+    [SerializeField]
+    private GameObject explosionPrefab;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Shoot"))
         {
             Destroy(collision.gameObject);
             Destroy(gameObject);
+            Instantiate(explosionPrefab, gameObject.transform.position, gameObject.transform.rotation);
         }
         
     }
