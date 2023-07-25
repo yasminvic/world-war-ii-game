a7be0e638efea63b404ebafb098b0a9f111490d6
Yasmin <yasminalvesdesouza27@gmail.com>
2023-07-25 19:09:48 -0300

    Fix: Retirando importações desnecessárias

--- Assets/Scripts/SystemDialogue/DialogueManager.cs
+++ Assets/Scripts/SystemDialogue/DialogueManager.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.Animations;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
