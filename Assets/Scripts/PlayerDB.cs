227b103e0e83ac1c63ce90f03f45e77f3e93ef2f
Yasmin <yasminalvesdesouza27@gmail.com>
2023-09-08 15:55:23 -0300

    Bug: Erro no Login

---
+++ Assets/Scripts/PlayerDB.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[Serializable]
+public class PlayerDB 
+{
+    [SerializeField]
+    public TMP_InputField username;
+    [SerializeField]
+    public TMP_InputField password;
+}
